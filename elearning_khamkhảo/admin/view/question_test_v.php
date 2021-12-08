ffc820c55f7069d810b575c75efb0fcc0b0ad00e
tranducanhtu1 <tranducanhtu.cntt@gmail.com>
2021-12-08 23:19:15 +0700

    check

--- elearning_khamkhảo/admin/view/question_test_v.php
+++
@@ -1,8 +0,0 @@
-<?php
-	foreach ($tests as $key => $test) {
-
-?>
-		<a href="index.php?action=question_course&param=question&id_course=<?php echo $course["id_course"]; ?>&id_test=<?php echo $id_test; ?>"><?php echo $test["name_test"]; ?></a><br>
-<?php
-	}
-?>
